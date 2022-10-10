731c0242eccb77c8c9916b76de115f80f643b7d8
Tocsmatts <toochukwuezeobi@gmail.com>
2022-10-10 14:38:13 -0700

    Adding file 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include "sort.h"
+
+/**
+ * selection_sort - selection sort function
+ *
+ * @array: array
+ * @size: array size
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
