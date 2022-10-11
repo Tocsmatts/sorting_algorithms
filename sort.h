65d529015d6126de49f10ee0306e219dfce0f4d0
Tocsmatts <toochukwuezeobi@gmail.com>
2022-10-11 09:03:13 -0700

    Updating file

--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void pos(int *a, int lb, int ub, int size);
+void shell_sort(int *array, size_t size);
 
 #endif
